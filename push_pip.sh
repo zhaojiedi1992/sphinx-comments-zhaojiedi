8c75341b9554eeaffeb6cf55b4675902f3e719a2
zhaojiedi <zhaojiedi@kuaishou.com>
2025-07-08 10:07:54 +0800

    add

--- push_pip.sh
+++ push_pip.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 function update_version(){
   FILE="$1"
-  version_string=$(grep -o 'version="[0-9]\+\.[0-9]\+\.[0-9]\+"' $FILE)
+  version_string=$(grep -o '__version__="[0-9]\+\.[0-9]\+\.[0-9]\+"' $FILE)
   version_number=$(echo $version_string | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
   IFS='.' read -r -a version_parts <<< "$version_number"
   new_patch_version=$((${version_parts[2]}+1))
   new_version="${version_parts[0]}.${version_parts[1]}.$new_patch_version"
   #sed -i "s@version=\"$version_number\"@version=\"$new_version\"@g" $FILE
-  sed -i.bak "s@version=\"$version_number\"@version=\"$new_version\"@g" $FILE
+  sed -i.bak "s@__version__=\"$version_number\"@__version__=\"$new_version\"@g" $FILE
 }
 update_version sphinx_comments_zhaojiedi/__init__.py
 source .venv/bin/activate 2>/dev/null 
